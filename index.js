d0e0c100c65901500ffbc2b4ececb6de4cd5edba
hex7c0 <carniellifrancesco@gmail.com>
2014-05-13 15:56:51 +0200

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * express-logger
+ * 
+ * @package express-logger
+ * @subpackage index
+ * @version 0.0.1
+ * @author hex7c0 <carniellifrancesco@gmail.com>
+ * @copyright GPLv3
+ */
