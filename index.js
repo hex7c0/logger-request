1583e343af7ec815cdee110fed6ab56aae7721df
hex7c0 <carniellifrancesco@gmail.com>
2014-06-08 10:26:10 +0200

    some improvements

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 /**
  * @file logger-request main
  * @module logger-request
- * @version 1.1.1
+ * @package logger-request
+ * @subpackage main
+ * @version 1.1.2
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -10,23 +12,15 @@
 
 /*
  * initialize module
- * 
  */
 // import
 try {
-    /**
-     * @global
-     * @requires winston
-     */
     var LOG = require('winston');
 } catch (MODULE_NOT_FOUND) {
     console.error(MODULE_NOT_FOUND);
     process.exit(1);
 }
 // load
-/**
- * @global
- */
 var log = null;
 
 /*
@@ -44,6 +38,7 @@ var log = null;
 function logging(req,res,next) {
 
     var start = process.hrtime();
+    var cache0 = log;
     var buffer = res.end;
     /**
      * end of job. Get response time and status code
@@ -54,20 +49,22 @@ function logging(req,res,next) {
      */
     res.end = function finale(chunk,encoding) {
 
+        var cache1 = cache0;
         res.end = buffer;
         res.end(chunk,encoding,function() {
 
             var diff = process.hrtime(start);
-            log('logger-request',{
+            cache1({
                 pid: process.pid,
                 method: req.method,
                 status: res.statusCode,
                 ip: req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress,
                 url: req.url,
+                original: req.originalUrl,
                 agent: req.headers['user-agent'],
                 lang: req.headers['accept-language'],
                 cookie: req.cookies,
-                response: diff[0] * 1e9 + diff[1],
+                response: (diff[0] * 1e9 + diff[1]) / 1000000,
             });
             return;
         });
@@ -91,12 +88,12 @@ function empty(req,res,next) {
 /**
  * option setting
  * 
- * @exports logger
- * @function logger
+ * @exports main
+ * @function main
  * @param {Object} options - various options. Check README.md
- * @return {function|object}
+ * @return {function|Object}
  */
-module.exports = function logger(options) {
+var main = module.exports = function(options) {
 
     var options = options || {};
     var my = {
@@ -105,27 +102,25 @@ module.exports = function logger(options) {
         level: String(options.level || 'info'),
         silent: Boolean(options.silent),
         colorize: Boolean(options.colorize),
-        timestamp: options.timestamp == false ? false : true,
+        timestamp: options.timestamp || true,
         filename: String(options.filename || 'route.log'),
-        maxsize: parseInt(options.maxsize) || 8388608,
-        maxFiles: parseInt(options.maxFiles) || null,
+        maxsize: Number(options.maxsize) || 8388608,
+        maxFiles: Number(options.maxFiles) || null,
         json: options.json == false ? false : true,
         raw: options.raw == false ? false : true,
         // override
-        console: options.console == true ? false : true,
+        console: Boolean(options.console),
+        standalone: Boolean(options.standalone),
     };
-    var standalone = options.standalone == true ? true : false;
 
     if (my.silent) {
-        // remove obsolete
         LOG = log = logging = null;
         return empty;
     }
-    // setting
-    LOG.loggers.add(my.logger,{
+    log = LOG.loggers.add(my.logger,{
         console: {
             level: my.level,
-            silent: my.console,
+            silent: !my.console,
             colorize: my.colorize,
             timestamp: my.timestamp,
             json: my.json,
@@ -141,13 +136,11 @@ module.exports = function logger(options) {
             maxFiles: my.maxFiles,
             json: my.json,
         }
-    });
-    log = LOG.loggers.get(my.logger)[my.level];
-    // remove obsolete
+    })[my.level];
     LOG = empty = null;
-    if (standalone) {
+    if (my.standalone) {
         logging = null;
         return log;
     }
     return logging;
-}
+};
