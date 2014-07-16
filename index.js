66450ae44a440d4eeb4079cd64495d6793c57bf5
hex7c0 <carniellifrancesco@gmail.com>
2014-07-16 10:33:38 +0200

    improve performance

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @module logger-request
  * @package logger-request
  * @subpackage main
- * @version 1.1.8
+ * @version 1.1.9
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -27,48 +27,48 @@ var log = null;
  * functions
  */
 /**
+ * end of job (closures). Get response time and status code
+ * 
+ * @function finale
+ * @param {Object} req - client request
+ * @param {Object} res - response to client
+ * @param {Array} start - hrtime
+ * @return
+ */
+function finale(req,res,start) {
+
+    var diff = process.hrtime(start);
+    log({
+        pid: process.pid,
+        method: req.method,
+        status: res.statusCode,
+        byte: req.socket._bytesDispatched,
+        ip: req.headers['x-forwarded-for'] || req.ip || req.headers['host'],
+        url: req.url,
+        agent: req.headers['user-agent'],
+        lang: req.headers['accept-language'],
+        cookie: req.cookies,
+        response: (diff[0] * 1e9 + diff[1]) / 1000000,
+    });
+    return;
+};
+
+/**
  * logging all route
  * 
  * @function logging
  * @param {Object} req - client request
  * @param {Object} res - response to client
  * @param {next} next - continue routes
- * @return
+ * @return {next}
  */
 function logging(req,res,next) {
 
     var start = process.hrtime();
     var buffer = res.end;
-    var log1 = log;
 
     /**
-     * end of job (closures). Get response time and status code
-     * 
-     * @function finale
-     * @return
-     */
-    var finale = function() {
-
-        var req1 = req;
-        var diff = process.hrtime(start);
-        log1({
-            pid: process.pid,
-            method: req1.method,
-            status: res.statusCode,
-            byte: req1.socket._bytesDispatched,
-            ip: req1.headers['x-forwarded-for'] || req1.ip
-                    || req1.headers['host'],
-            url: req1.url,
-            agent: req1.headers['user-agent'],
-            lang: req1.headers['accept-language'],
-            cookie: req1.cookies,
-            response: (diff[0] * 1e9 + diff[1]) / 1000000,
-        });
-        return;
-    };
-
-    /**
-     * middle of job (closures). Set right end function
+     * middle of job. Set right end function
      * 
      * @function
      * @param {String} chunk - data sent
@@ -77,19 +77,16 @@ function logging(req,res,next) {
      */
     res.end = function(chunk,encoding) {
 
-        finale()
         res.end = buffer;
-        res.end(chunk,encoding)
-        // res.end(chunk,encoding,finale) //callback available only with node 0.11
+        res.end(chunk,encoding);
+        // res.end(chunk,encoding,finale) // callback available only with node 0.11
+        finale(req,res,start); // write after sending all stuff, instead of callback
         return;
     };
 
-    try {
-        return next();
-    } catch (TypeError) {
-        return;
-    }
+    return next();
 }
+
 /**
  * option setting
  * 
@@ -118,7 +115,7 @@ module.exports = function logger(options) {
     };
 
     if (my.silent) {
-        LOG = log = logging = null;
+        LOG = log = logging = finale = null;
         return function(req,res,next) {
 
             return next();
@@ -146,7 +143,7 @@ module.exports = function logger(options) {
     })[my.level];
     LOG = null;
     if (my.standalone) {
-        logging = null;
+        logging = finale = null;
         return log;
     }
     return logging;
