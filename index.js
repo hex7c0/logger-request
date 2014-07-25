ac35787a19b576ed1608ae8177042ecefac8e7c9
hex7c0 <carniellifrancesco@gmail.com>
2014-07-25 11:50:45 +0200

    rewrite for multiple require

--- index.js
+++ index.js
@@ -4,175 +4,180 @@
  * @module logger-request
  * @package logger-request
  * @subpackage main
- * @version 2.0.3
+ * @version 2.1.0
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
  */
 
 /*
- * initialize module
- */
-// import
-try {
-    var LOG = require('winston');
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
-// load
-var oi, log;
-var storyReq = 0, storyRes = 0;
-
-/*
  * functions
  */
 /**
- * end of job (closures). Get response time and status code
+ * function wrapper for multiple require
  * 
- * @function finale
- * @param {Object} req - client request
- * @param {Integr} code - statusCode
- * @param {Array} start - hrtime
- * @return
+ * @function wrapper
+ * @param {Function} logger - logging function
+ * @param {Object} cst - custom object parsed
+ * @return {Function}
  */
-function finale(req,statusCode,start) {
+function wrapper(logger,cst) {
 
-    var diff = process.hrtime(start);
-    return log('',oi(req,statusCode,(diff[0] * 1e9 + diff[1]) / 1000000));
-}
+    var mod;
+    var log = logger
+    var oi = info(cst);
+    var storyReq = 0, storyRes = 0;
 
-/**
- * builder of option
- * 
- * @function info
- * @param {Object} my - user options
- * @return {Object}
- */
-function info(my) {
+    /**
+     * builder of option
+     * 
+     * @function info
+     * @param {Object} my - user options
+     * @return {Object}
+     */
+    function info(my) {
 
-    var out = Object.create(null);
-    var promise = new Array();
+        var out = Object.create(null);
+        var promise = new Array();
 
-    if (my.pid) {
-        promise.push(['pid',function(req) {
+        if (my.pid) {
+            promise.push(['pid',function(req) {
 
-            return process.pid;
-        }]);
-    }
-    if (my.bytesReq) {
-        promise.push(['bytesReq',function(req) {
-
-            var s = req.socket.bytesRead - storyReq;
-            storyReq = req.socket.bytesRead;
-            return s;
-        }]);
-    }
-    if (my.bytesRes) {
-        promise.push(['bytesRes',function(req) {
-
-            var s = req.socket._bytesDispatched - storyRes;
-            storyRes = req.socket._bytesDispatched;
-            return s;
-        }]);
-    }
-    if (my.referrer) {
-        promise.push(['referrer',function(req) {
-
-            return req.headers['referer'] || req.headers['referrer'];
-        }]);
-    }
-    if (my.auth) {
-        LOG = require('basic-authentication')({
-            legacy: true
-        });
-        promise.push(['auth',function(req) {
-
-            return LOG(req).user;
-        }]);
-    }
-    if (my.agent) {
-        promise.push(['agent',function(req) {
-
-            return req.headers['user-agent'];
-        }]);
-    }
-    if (my.lang) {
-        promise.push(['lang',function(req) {
-
-            return req.headers['accept-language'];
-        }]);
-    }
-    if (my.cookie) {
-        promise.push(['cookie',function(req) {
-
-            return req.cookies;
-        }]);
-    }
-    if (my.version) {
-        promise.push(['version',function(req) {
-
-            return req.httpVersionMajor + '.' + req.httpVersionMinor;
-        }]);
-    }
-
-    return function(req,statusCode,start) {
-
-        var prom = promise;
-        out.ip = req.headers['x-forwarded-for'] || req.ip
-                || req.connection.remoteAddress;
-        out.method = req.method;
-        out.status = statusCode;
-        out.url = req.url;
-        out.response = start.toFixed(3);
-        for (var i = 0, ii = prom.length; i < ii; i++) {
-            var p = prom[i];
-            out[p[0]] = p[1](req);
+                return process.pid;
+            }]);
         }
-        return out;
-    };
-}
+        if (my.bytesReq) {
+            promise.push(['bytesReq',function(req) {
 
-/**
- * logging all route
- * 
- * @function logging
- * @param {Object} req - client request
- * @param {Object} res - response to client
- * @param {next} next - continue routes
- * @return {next}
- */
-function logging(req,res,next) {
+                var s = req.socket.bytesRead - storyReq;
+                storyReq = req.socket.bytesRead;
+                return s;
+            }]);
+        }
+        if (my.bytesRes) {
+            promise.push(['bytesRes',function(req) {
 
-    var start = process.hrtime();
-    if (res._headerSent) { // function
-        finale(req,res.statusCode,start); // after res.end()
-    } else { // middleware
-        var buffer = res.end;
-        var fin = finale;
+                var s = req.socket._bytesDispatched - storyRes;
+                storyRes = req.socket._bytesDispatched;
+                return s;
+            }]);
+        }
+        if (my.referrer) {
+            promise.push(['referrer',function(req) {
 
-        /**
-         * middle of job. Set right end function (closure)
-         * 
-         * @function
-         * @param {String} chunk - data sent
-         * @param {String} encoding - data encoding
-         * @return {Boolean}
-         */
-        res.end = function(chunk,encoding) {
+                return req.headers['referer'] || req.headers['referrer'];
+            }]);
+        }
+        if (my.auth) {
+            mod = require('basic-authentication')({
+                legacy: true
+            });
+            promise.push(['auth',function(req) {
 
-            res.end = buffer;
-            var b = res.end(chunk,encoding);
-            // res.end(chunk,encoding,finale) // callback available only with node 0.11
-            fin(req,res.statusCode,start); // write after sending all stuff, instead of callback
-            return b;
+                return mod(req).user;
+            }]);
+        }
+        if (my.agent) {
+            promise.push(['agent',function(req) {
+
+                return req.headers['user-agent'];
+            }]);
+        }
+        if (my.lang) {
+            promise.push(['lang',function(req) {
+
+                return req.headers['accept-language'];
+            }]);
+        }
+        if (my.cookie) {
+            promise.push(['cookie',function(req) {
+
+                return req.cookies;
+            }]);
+        }
+        if (my.version) {
+            promise.push(['version',function(req) {
+
+                return req.httpVersionMajor + '.' + req.httpVersionMinor;
+            }]);
+        }
+
+        return function(req,statusCode,start) {
+
+            var re = req.req || req;
+            var prom = promise;
+            out.ip = re.headers['x-forwarded-for'] || re.ip
+                    || re.connection.remoteAddress;
+            out.method = re.method;
+            out.status = statusCode;
+            out.url = re.url;
+            out.response = start.toFixed(3);
+            for (var i = 0, ii = prom.length; i < ii; i++) {
+                var p = prom[i];
+                out[p[0]] = p[1](re);
+            }
+            return out;
         };
     }
 
-    try {
-        return next();
-    } catch (TypeError) {
-        return;
+    /**
+     * end of job (closures). Get response time and status code
+     * 
+     * @function finale
+     * @param {Object} req - client request
+     * @param {Integr} code - statusCode
+     * @param {Array} start - hrtime
+     * @return
+     */
+    function finale(req,statusCode,start) {
+
+        var diff = process.hrtime(start);
+        return log(oi(req,statusCode,(diff[0] * 1e9 + diff[1]) / 1000000));
+    }
+
+    /**
+     * logging all route
+     * 
+     * @function logging
+     * @param {Object} req - client request
+     * @param {Object} res - response to client
+     * @param {next} next - continue routes
+     * @return {next}
+     */
+    return function logging(req,res,next) {
+
+        var start = process.hrtime();
+
+        if (res._headerSent) { // function
+            finale(req,res.statusCode,start); // after res.end()
+        } else { // middleware
+            var buffer = res.end;
+            var fin = finale;
+
+            /**
+             * middle of job. Set right end function (closure)
+             * 
+             * @function
+             * @param {String} chunk - data sent
+             * @param {String} encoding - data encoding
+             * @return {Boolean}
+             */
+            res.end = function(chunk,encoding) {
+
+                res.end = buffer;
+                var b = res.end(chunk,encoding);
+                // res.end(chunk,encoding,finale) // callback available only with node 0.11
+                fin(req,res.statusCode,start); // write after sending all stuff, instead of
+                // callback
+                return b;
+            };
+        }
+
+        try {
+            return next();
+        } catch (TypeError) {
+            return;
+        }
     }
 }
 
@@ -206,7 +211,7 @@ module.exports = function logger(options) {
         json: options.winston.json == false ? false : true,
         raw: options.winston.raw == false ? false : true,
     };
-    log = LOG.loggers.add(my.logger,{
+    var log = require('winston').loggers.add(my.logger,{
         console: {
             level: my.winston.level,
             silent: my.console,
@@ -227,6 +232,10 @@ module.exports = function logger(options) {
         }
     })[my.winston.level];
 
+    if (Boolean(options.standalone)) {
+        return log;
+    }
+
     // custom
     options.custom = options.custom || Object.create(null);
     my.custom = {
@@ -240,10 +249,6 @@ module.exports = function logger(options) {
         cookie: Boolean(options.custom.cookie),
         version: Boolean(options.custom.version),
     };
-    oi = info(my.custom);
 
-    if (Boolean(options.standalone)) {
-        return log;
-    }
-    return logging;
+    return wrapper(log,my.custom);
 };
