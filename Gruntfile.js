6063844eabbd9ae1c40c9e3bfd9ef16042d30dcf
hex7c0 <carniellifrancesco@gmail.com>
2014-09-01 18:21:21 +0200

    3.0.6

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,18 +49,11 @@ module.exports = function(grunt) {
 
         endline: {
             target: {
+                options: {
+                    except: 'node_modules'
+                },
                 files: [ {
-                    src: 'examples/**/*.js'
-                }, {
-                    src: 'public/**/*.js'
-                }, {
-                    src: 'module/**/*.js'
-                }, {
-                    src: 'lib/**/*.js'
-                }, {
-                    src: 'test/**/*.js'
-                }, {
-                    'index.min.js': 'index.min.js'
+                    src: './**/*.js'
                 } ]
             }
         }
