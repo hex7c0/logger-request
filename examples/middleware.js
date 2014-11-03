8c94d760e6777b8fadf79f134cb5f7f005487bc8
hex7c0 <carniellifrancesco@gmail.com>
2014-11-03 10:33:23 +0100

    update require

--- examples/middleware.js
+++ examples/middleware.js
@@ -14,8 +14,7 @@
  */
 // import
 try {
-    var logger = require('../index.min.js'); // use require('logger-request')
-                                                // instead
+    var logger = require('..'); // use require('logger-request') instead
     var app = require('express')();
 } catch (MODULE_NOT_FOUND) {
     console.error(MODULE_NOT_FOUND);
