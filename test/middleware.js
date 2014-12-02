326c073d536f7a1ba77050f3f087dcbb1b89cf30
hex7c0 <carniellifrancesco@gmail.com>
2014-12-02 21:07:24 +0100

    add callback test

--- test/middleware.js
+++ test/middleware.js
@@ -14,14 +14,14 @@
  */
 // import
 try {
-    var logger = require('..'); // use require('logger-request')
-    var app = require('express')();
-    var request = require('supertest');
-    var assert = require('assert');
-    var fs = require('fs');
+  var logger = require('..'); // use require('logger-request')
+  var app = require('express')();
+  var request = require('supertest');
+  var assert = require('assert');
+  var fs = require('fs');
 } catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
+  console.error(MODULE_NOT_FOUND);
+  process.exit(1);
 }
 
 /*
@@ -29,49 +29,57 @@ try {
  */
 describe('0.11 fix', function() {
 
-    before(function(done) {
+  before(function(done) {
 
-        var log = logger({
-            filename: 'f.log',
-            winston: {
-                logger: 'f'
-            }
-        });
-        app.get('/', function(req, res) {
+    var log = logger({
+      filename: 'f.log',
+      custom: {
+        callback: function(req) {
 
-            res.status(403).end('hello world!');
-            log(req, res, function() {
-
-                return;
-            });
-        });
-        done();
+          return req.res._headers['x-pippo'];
+        }
+      },
+      winston: {
+        logger: 'f'
+      }
     });
+    app.get('/', function(req, res) {
 
-    it('middleware - should read log of GET "/" 403 after end', function(done) {
+      res.setHeader('x-pippo', 'pluto');
+      res.status(403).end('hello world!');
+      log(req, res, function() {
 
-        request(app).get('/').expect(403).end(function(err, res) {
-
-            setTimeout(function() {
-
-                fs.readFile('f.log', {
-                    encoding: 'utf8'
-                }, function(err, data) {
-
-                    if (err)
-                        return done(err);
-                    var d = JSON.parse(data);
-                    assert.deepEqual(d.method, 'GET', 'method');
-                    assert.deepEqual(d.status, 403, 'status code');
-                    assert.deepEqual(d.url, '/', 'url');
-                    assert.deepEqual(d.message, 'f', 'logger');
-                    assert.deepEqual(d.level, 'info', 'log level');
-                    fs.unlink('f.log', function() {
-
-                        done();
-                    });
-                });
-            }, 25);
-        });
+        return;
+      });
     });
+    done();
+  });
+
+  it('middleware - should read log of GET "/" 403 after end', function(done) {
+
+    request(app).get('/').expect(403).end(function(err, res) {
+
+      setTimeout(function() {
+
+        fs.readFile('f.log', {
+          encoding: 'utf8'
+        }, function(err, data) {
+
+          if (err)
+            return done(err);
+          var d = JSON.parse(data);
+          assert.deepEqual(d.method, 'GET', 'method');
+          assert.deepEqual(d.status, 403, 'status code');
+          assert.deepEqual(d.url, '/', 'url');
+          assert.deepEqual(d.message, 'f', 'logger');
+          assert.deepEqual(d.level, 'info', 'log level');
+          assert.deepEqual(d.callback, 'pluto', 'callback');
+          fs.unlink('f.log', function() {
+
+            done();
+          });
+        });
+      }, 25);
+    });
+  });
 });
